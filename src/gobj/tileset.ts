675fec47dca623beeb087e9bbae9f7a659c8d0ae
nicfv <nfmventura@gmail.com>
2023-09-25 22:40:45 -0700

    Finally get tileset at a state that I like!

--- src/gobj/tileset.ts
+++ src/gobj/tileset.ts
@@ -2,25 +2,42 @@ import { Size } from '../lib/size';
 import { Vec2 } from '../lib/vec';
 
 /**
- * Generate a sequence of images from a tileset source.
+ * Contains a sequence of uniformly-sized images from a tileset source.
  */
 export class Tileset {
     private readonly tileset: HTMLImageElement;
-    constructor(source: string, public readonly tileSize: Size) {
+    private readonly tiles: Array<Array<Tile>>;
+    /**
+     * Generate a sequence of uniformly-sized images from a tileset source.
+     * @param source The image source URI/URL
+     * @param tileSize The uniform size of each tile
+     */
+    constructor(source: string, tileSize: Size) {
         this.tileset = new Image();
         this.tileset.src = source;
+        this.tiles = [];
+        for (let x = 0; x < this.tileset.width; x += tileSize.width) {
+            this.tiles.push([]);
+            for (let y = 0; y < this.tileset.height; y += tileSize.height) {
+                this.tiles[x].push(new Tile((context, position) => context.drawImage(this.tileset, x, y, tileSize.width, tileSize.height, position.x, position.y, tileSize.width, tileSize.height)));
+            }
+        }
     }
-    public drawTile(context: CanvasRenderingContext2D, coordinates: Vec2, position: Vec2): void {
-        context.drawImage(this.tileset, coordinates.x * this.tileSize.width, coordinates.y * this.tileSize.height, this.tileSize.width, this.tileSize.height, position.x, position.y, this.tileSize.width, this.tileSize.height)
-    }
+    /**
+     * Return the tile from the specified position in the tileset.
+     */
     public getTile(coordinates: Vec2): Tile {
-        return new Tile(this, coordinates);
+        return this.tiles[coordinates.x][coordinates.y];
     }
 }
 
+/**
+ * Represents a single drawable image element from a tileset.
+ */
 export class Tile {
-    constructor(private readonly tileset: Tileset, private readonly coordinates: Vec2) { }
-    public drawTile(context: CanvasRenderingContext2D, position: Vec2): void {
-        this.tileset.drawTile(context, this.coordinates, position);
-    }
+    /**
+     * Initialize a new instance of `Tile`. Should only be called in the `Tileset` class.
+     * @param draw Draw the tile at the specified position on the game window.
+     */
+    constructor(public readonly draw: (context: CanvasRenderingContext2D, position: Vec2) => void) { }
 }
